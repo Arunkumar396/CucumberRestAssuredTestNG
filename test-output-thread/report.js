38d9e31df4e305fe4e120ba93afaadc0a8197783
Arunkumar396 <arunmanasu199465@gmail.com>
2025-04-26 17:34:29 +0530

    Updated pom.xml to use Java 17

---
+++ test-output-thread/report.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "e6d9fc49-ab88-4882-a47c-8707fb78572a",
+    "feature": "Validating Place API\u0027s",
+    "scenario": "Verify if Place is being Succesfully added using AddPlaceAPI",
+    "start": 1745668387812,
+    "group": 15,
+    "content": "",
+    "tags": "",
+    "end": 1745668399364,
+    "className": "passed"
+  },
+  {
+    "id": "038eaa5a-8df1-4e88-a78b-1a356f0a5ac7",
+    "feature": "Validating Place API\u0027s",
+    "scenario": "Verify if Delete Place functionality is working",
+    "start": 1745668399414,
+    "group": 15,
+    "content": "",
+    "tags": "",
+    "end": 1745668405826,
+    "className": "passed"
+  },
+  {
+    "id": "0c946c45-aaf3-4d4e-8bc6-d324e5b3eff9",
+    "feature": "Login Functionality for OpenCart E-commerce Website",
+    "scenario": "Successful login with valid credentials",
+    "start": 1745668405855,
+    "group": 15,
+    "content": "",
+    "tags": "",
+    "end": 1745668414247,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 15,
+    "content": "Thread[TestNG-test\u003dCucumber Tests-1,5,main]"
+  }
+]);
+});
