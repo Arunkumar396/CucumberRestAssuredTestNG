8a1032c5693f0f77f67147426085c8c746e92e85
Arunkumar396 <arunmanasu199465@gmail.com>
2025-04-26 21:29:58 +0530

    'updated'

---
+++ test-output-thread/report.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "a1dd4a04-7674-4d27-b29f-1f1d61b3a40d",
+    "feature": "Validating Place API\u0027s",
+    "scenario": "Verify if Delete Place functionality is working",
+    "start": 1745682800720,
+    "group": 16,
+    "content": "",
+    "tags": "",
+    "end": 1745682806162,
+    "className": "passed"
+  },
+  {
+    "id": "46eecbf8-e4cf-4fc5-a49e-8d6b4123ce7d",
+    "feature": "Login Functionality for OpenCart E-commerce Website",
+    "scenario": "Successful login with valid credentials",
+    "start": 1745682806172,
+    "group": 16,
+    "content": "",
+    "tags": "",
+    "end": 1745682814093,
+    "className": "passed"
+  },
+  {
+    "id": "d3a8f4ed-ec9d-41f2-992a-35f4895a5965",
+    "feature": "Validating Place API\u0027s",
+    "scenario": "Verify if Place is being Succesfully added using AddPlaceAPI",
+    "start": 1745682788530,
+    "group": 16,
+    "content": "",
+    "tags": "",
+    "end": 1745682800702,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 16,
+    "content": "Thread[TestNG-test\u003dCucumber Tests-1,5,main]"
+  }
+]);
+});
